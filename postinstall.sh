331ea3bc495cdd8e7796ee3c6c91fc516bb96a20
Jamy Golden <jamygolden@gmail.com>
2015-12-10 11:55:14 +0200

    Add sh install coments

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,18 @@
+export DEBIAN_FRONTEND=noninteractive
+
+echo "# Installing perlbrew"
 \wget -O - http://install.perlbrew.pl | bash
 echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
 source ~/.bash_profile
 
+echo "# Installing perl"
 perlbrew install perl-5.16.3
 perlbrew switch perl-5.16.3
 
+echo "# Installing cpan"
 curl -L https://cpanmin.us | perl - App::cpanminus
 
+echo "# Installing duckpan"
 curl http://duckpan.org/install.pl | perl
 
 # Shouldn't need to do this:
--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
+echo "# Update Ubuntu repo"
 apt-get update
+
+echo "# Install Ubuntu packages"
 apt-get -q -y install build-essential autoconf libssl-dev git git-core
