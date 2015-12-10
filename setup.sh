c545b74b1cb71995ba533965e0dcc513341c9503
Jamy Golden <jamygolden@gmail.com>
2015-12-10 11:57:17 +0200

    Add `curl` to setup script

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ echo "# Update Ubuntu repo"
 apt-get update
 
 echo "# Install Ubuntu packages"
-apt-get -q -y install build-essential autoconf libssl-dev git git-core
+apt-get -q -y install build-essential autoconf libssl-dev git git-core curl
